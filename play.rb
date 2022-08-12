39acddfebac4bd410d6896f116b68fc68a7f5ef5
takeru <post8zntyn@gmail.com>
2022-08-12 00:51:09 +0000

    Ruby課題じゃんけんアプリ提出081222

--- play.rb
+++ play.rb
@@ -1,75 +1,108 @@
 puts "じゃんけん"
 
-¥#jyankenメソッドを定義する
+#じゃんけん====================================================
+#jyankenメソッドを定義する
   def jyanken
     
     puts "自分が出す手1(グー)2(チョキ)3(パー)"
-    #my_hand変数に自分が選択した手を代入
     my_choice = gets.to_i
-    #配列0には、空を入れる←使用しないから...
     type_hand= ["","グー","チョキ","パー"]
-    #enemy_choice変数にrandメソッドにて出力した1〜3のランダムな数字を代入
     enemy_choice = rand(1..3)
-    #それぞれの手を表示
     puts "自分の手：#{type_hand[my_choice]}"
     puts "相手の手：#{type_hand[enemy_choice]}"
     
-    #if文にて勝敗による条件分岐
     if my_choice == enemy_choice
-      #自分の手と相手の手が同じ時、あいこと出力
       puts "あいこで"
       return true
     elsif my_choice == 1 && enemy_choice == 2 || my_choice == 2 && enemy_choice == 3 || my_choice == 3 && enemy_choice == 1
-      #自分が勝ちの時の条件は、勝ちと出力
       puts "あなたの勝ちです"
+      #グローバル変数で勝ち負けを指定..本来は保守性・可読性の観点から使用は推奨されていない
+      #ここを、ローカル変数で置き換えたい...
+      $win_or_lose = "win"
+      puts $win_or_lose
       return false
-      @win_or_lose = 1
     else
-      #上記以外のパターンは負けと出力
       puts "あなたの負けです"
+      $win_or_lose = "lose"
+      puts $win_or_lose
       return false
-      @win_or_lose = 2
     end
     
   end
   
+  
+  #あいこの場合、じゃんけんメソッドをループ
   #re_jyanken にtrueを代入
   re_jyanken = true
   
   #re_jynakenがtrueの時？re_jynaknen = jyanken 処理を行う。
-  #仮にjyankenがtrueの時、もう一度while文により繰り返す。
+  #jyankenの戻り値がtrueの時、jyankenメソッドを繰り返す。
   while re_jyanken
     re_jyanken = jyanken
   end
-  
-  
+
+#==============================================================
+
   #あっちむいてホイ============================================
   #hoihoiメソッドを定義する
   
   def hoihoi
     puts "あっち向いて"
     
-    @win_or_lose
-  
-  if @win_or_lose == 1
-    puts "じゃんけんで勝ちました"
-    puts "指を指す方向を決めます"
-  elsif @win_or_lose == 2
-    puts "じゃんけんで負けました"
-    puts "向く方法を決めます"
-  else
-    puts "じゃんけんメソッドと同期できていません"
-  end
+    type_direction = ["","上","右","下","左"]
+    enemy_direction = rand(1..4)
+    
+    
+    if $win_or_lose == "win"
+      puts "指を差す方向を決めます"
+      puts "1(上)2(右)3(下)4(左)"
+      
+      pick_direction = gets.to_i
+      
+      puts "自分が差した方向：#{type_direction[pick_direction]}"
+      puts "相手が向いた方向：#{type_direction[enemy_direction]}"
+      
+      if  pick_direction == enemy_direction
+        puts "すごい！あなたの勝ちです！！"
+      else
+        puts "もう一度"
+        return true
+      end
+      
+    elsif $win_or_lose == "lose"
+      puts "向く方法を決めます"
+      puts "1(上)2(右)3(下)4(左)"
+      
+      pick_direction = gets.to_i
+
+      puts "自分が向いた方向：#{type_direction[pick_direction]}"
+      puts "相手が差した方向：#{type_direction[enemy_direction]}"
+      
+      if  pick_direction == enemy_direction
+        puts "残念..あなたの負けです.."
+      else
+        puts "もう一度"
+        return true
+      end
+      
+      
+    else
+      puts "じゃんけんメソッドと同期できていません"
+    end
     
   end
   
-  next_game = false
-  
+  #じゃんけんの勝敗からhoihoiメソッドへの移行
+  next_game = true
   
   while hoihoi
     while next_game
-      next_game = jyanken
+      next_game = jyarubynken
     end
   end
   
   #============================================================
+
+
+
+
